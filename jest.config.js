11f0ed0dddd799155a81da9620962402a1dcc3e8
Glitch (techeverri-pow) <none>
2020-10-14 22:13:28 +0000

    🎈🎩 Updated with Glitch

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   roots: ["<rootDir>/test"],
   moduleNameMapper: {
-    "/src/(.*)": "<rootDir>/src/$1",
+    "/src/(.*)": "<rootDir>/src/$1"
   },
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
+    "^.+\\.tsx?$": "ts-jest"
+  }
 };
